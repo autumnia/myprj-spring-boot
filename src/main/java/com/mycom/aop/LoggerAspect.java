04501ec16c274504b6b31d3334da0ab72005b427
autumnia <autumnya@gmail.com>
2020-01-07 21:11:08 +0900

    aop 추가

---
+++ src/main/java/com/mycom/aop/LoggerAspect.java
@@ -0,0 +1,28 @@
+package com.mycom.aop;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+@Slf4j
+public class LoggerAspect {
+
+    @Around("execution(* mycom..controllers.*Controller.*(..)) or execution(* mycom..services.*service.*(..)) ")
+    public Object logPrint(ProceedingJoinPoint joinPoint) throws Throwable {
+        String type = "";
+        String name = joinPoint.getSignature().getDeclaringTypeName();
+        if (name.indexOf("Controller") > -1) {
+            type = "Controller  \t:  ";
+        }
+        else if (name.indexOf("Service") > -1) {
+            type = "ServiceImpl  \t:  ";
+        }
+
+        log.debug(type + name + "." + joinPoint.getSignature().getName() + "()");
+        return joinPoint.proceed();
+    }
+}
