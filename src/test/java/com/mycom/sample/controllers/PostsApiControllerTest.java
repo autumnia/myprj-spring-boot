12e00461300cbe2b40dbee6842cb4730ba2f743b
autumnia <autumnya@gmail.com>
2020-01-19 13:29:08 +0900

    소스수정

--- src/test/java/com/mycom/sample/controllers/PostsApiControllerTest.java
+++ src/test/java/com/mycom/sample/controllers/PostsApiControllerTest.java
@@ -14,7 +14,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.MediaType;
-//import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -23,7 +23,7 @@ import org.springframework.web.context.WebApplicationContext;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-//import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -51,7 +51,7 @@ public class PostsApiControllerTest {
     public void setup() {
         mvc = MockMvcBuilders
                 .webAppContextSetup(context)
-//                .apply(springSecurity())
+                .apply(springSecurity())
                 .build();
 
         System.out.println( "port: " + port ) ;
@@ -63,7 +63,7 @@ public class PostsApiControllerTest {
     }
 
     @Test
-//    @WithMockUser(roles="USER")
+    @WithMockUser(roles="USER")
     public void Posts_등록된다() throws Exception {
         //given
         String title = "title";
@@ -89,7 +89,7 @@ public class PostsApiControllerTest {
     }
 
     @Test
-//    @WithMockUser(roles="USER")
+    @WithMockUser(roles="USER")
     public void Posts_수정된다() throws Exception {
         //given
         Posts savedPosts = postsRepository.save(Posts.builder()
