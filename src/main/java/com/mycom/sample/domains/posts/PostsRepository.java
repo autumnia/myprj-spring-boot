af2360f97c3cbc11ab284471b79bb046c970cab5
autumnya@gmail.com <rlagoems00!>
2020-01-28 22:54:07 +0900

    @Repository 어노테이션 추가

--- src/main/java/com/mycom/sample/domains/posts/PostsRepository.java
+++ src/main/java/com/mycom/sample/domains/posts/PostsRepository.java
@@ -3,8 +3,11 @@ package com.mycom.sample.domains.posts;
 import com.mycom.sample.domains.posts.Posts;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
 import java.util.List;
 
+@Repository
 public interface PostsRepository extends JpaRepository<Posts, Long> {
     @Query("SELECT p FROM Posts p ORDER BY p.id DESC")
     List<Posts> findAllDesc();
