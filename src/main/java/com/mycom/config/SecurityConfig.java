2a2b0fa260f4aea0a8fe78c9c406cc7fa26048d3
autumnya@gmail.com <rlagoems00!>
2020-02-02 23:42:58 +0900

    csrf 추가

--- src/main/java/com/mycom/config/SecurityConfig.java
+++ src/main/java/com/mycom/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import com.mycom.sample.domains.user.Role;
 import lombok.*;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.*;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @RequiredArgsConstructor
 @EnableWebSecurity
@@ -14,15 +15,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
+            http
                 // h2-console 화면을 보기 위해 csrf disable 시킴  나중에는 제거 필요함
-                .csrf().disable()
-                .headers().frameOptions().disable()
-                .and()
-                    .authorizeRequests()
-                    .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**", "/profile").permitAll()
+                //.csrf().disable()
+                //.headers().frameOptions().disable()
+                //.and()
+                .authorizeRequests()
+                    .antMatchers("/", "/css/**", "/images/**", "/js/**", "/profile").permitAll()
                     .antMatchers("/api/v1/**").hasRole(Role.USER.name())
                     .anyRequest().authenticated() // 위 설정된 값을 제외한 모든 url 적용
+                .and()
+                .csrf()
+                    .csrfTokenRepository( CookieCsrfTokenRepository.withHttpOnlyFalse() )
                 .and()
                     .logout()
                         .logoutSuccessUrl("/") // 로그아웃 성공 후 돌아갈 주소
