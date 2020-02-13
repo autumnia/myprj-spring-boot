315313c6147d9ab76e24db52f2048dfc448be25a
autumnya@gmail.com <rlagoems00!>
2020-02-13 23:25:20 +0900

    config에 로거 인터셉터 등록

--- src/main/java/com/mycom/config/WebConfig.java
+++ src/main/java/com/mycom/config/WebConfig.java
@@ -9,15 +9,23 @@ import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.mycom.common.HtmlCharacterEscapes;
+import com.mycom.interceptor.*;
 
 import java.util.List;
 
 @RequiredArgsConstructor
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    // 로거 인터셉터
+    @Override
+    public void addInterceptors(InterceptorRegistry registry){
+        registry.addInterceptor(new LoggerInterceptor());
+    }
 
     private final LoginUserArgumentResolver loginUserArgumentResolver;
 
