7d801abcc7461db416e40e0943f4c3e283eb4367
autumnya@gmail.com <rlagoems00!>
2020-02-14 00:15:57 +0900

    config에 로거 인터셉터 등록

--- src/main/java/com/mycom/interceptor/LoggerInterceptor.java
+++ src/main/java/com/mycom/interceptor/LoggerInterceptor.java
@@ -12,16 +12,17 @@ public class LoggerInterceptor extends HandlerInterceptorAdapter{
 
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-		log.debug("");
+		log.debug("\n");
 		log.debug("======================================          START         ======================================");
 		log.debug(" Request URI \t:  " + request.getRequestURI());
+
 		return super.preHandle(request, response, handler);
 	}
 	
 	@Override
 	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
 		log.debug("======================================           END          ======================================\n");
-		log.debug("");
+		log.debug("\n");
 	}
 }
 
