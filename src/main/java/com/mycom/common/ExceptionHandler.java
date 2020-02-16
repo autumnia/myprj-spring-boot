26533805b46508b87103677eabe0bab5e8f4bb82
autumnya@gmail.com <rlagoems00!>
2020-02-16 10:13:06 +0900

    ExceptionHandler 추가

---
+++ src/main/java/com/mycom/common/ExceptionHandler.java
@@ -0,0 +1,33 @@
+package com.mycom.common;
+
+import javax.servlet.http.HttpServletRequest;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.ModelAndView;
+
+@ControllerAdvice
+@Slf4j
+public class ExceptionHandler {
+	
+	@org.springframework.web.bind.annotation.ExceptionHandler(Exception.class)
+	public ModelAndView defaultExceptionHandler(HttpServletRequest request, Exception exception){
+		ModelAndView mv = new ModelAndView("/error/error");
+		mv.addObject("exception", exception);
+		
+		log.error("defaultExceptionHandler", exception);
+		
+		return mv;
+	}
+
+	@org.springframework.web.bind.annotation.ExceptionHandler(Exception.class)
+	public ModelAndView nullPointExceptionHandler( HttpServletRequest request, NullPointerException exception ){
+		ModelAndView mv = new ModelAndView("/error/error");
+		mv.addObject("exception", exception);
+
+		log.error("NullPointException", exception);
+
+		return mv;
+	}
+
+}
