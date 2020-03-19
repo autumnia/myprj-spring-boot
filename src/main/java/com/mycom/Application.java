bed6306f2c6e8920c9e24f5ddeed54d8d7a32974
autumnya@gmail.com <rlagoems00!>
2020-03-19 23:49:30 +0900

    익셉션 핸들러 수정

--- src/main/java/com/mycom/Application.java
+++ src/main/java/com/mycom/Application.java
@@ -4,10 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 
+
+
 @SpringBootApplication
 @EnableAutoConfiguration
 public class Application {
     public static void main( String[] args ) {
+
         SpringApplication.run( Application.class, args );
     }
 }
