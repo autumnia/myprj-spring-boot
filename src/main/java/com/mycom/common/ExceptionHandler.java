70436ef91016e50ebca0a42ee1a0a01dcedca126
autumnya@gmail.com <rlagoems00!>
2020-03-19 23:01:47 +0900

    익셉션 핸들러 수정2

--- src/main/java/com/mycom/common/ExceptionHandler.java
+++ src/main/java/com/mycom/common/ExceptionHandler.java
@@ -6,10 +6,43 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.io.IOException;
+import java.sql.SQLException;
+
 @ControllerAdvice
 @Slf4j
 public class ExceptionHandler {
-	
+	@org.springframework.web.bind.annotation.ExceptionHandler(SQLException.class)
+	public ModelAndView defaultExceptionHandler(HttpServletRequest request, SQLException exception){
+		ModelAndView mv = new ModelAndView("/errors/error");
+		mv.addObject("exception", exception);
+
+		log.error("defaultExceptionHandler", exception);
+
+		return mv;
+	}
+
+
+	@org.springframework.web.bind.annotation.ExceptionHandler(NullPointerException.class)
+	public ModelAndView defaultExceptionHandler(HttpServletRequest request, NullPointerException exception){
+		ModelAndView mv = new ModelAndView("/errors/error");
+		mv.addObject("exception", exception);
+
+		log.error("defaultExceptionHandler", exception);
+
+		return mv;
+	}
+
+	@org.springframework.web.bind.annotation.ExceptionHandler(IOException.class)
+	public ModelAndView defaultExceptionHandler(HttpServletRequest request, IOException exception){
+		ModelAndView mv = new ModelAndView("/errors/error");
+		mv.addObject("exception", exception);
+
+		log.error("defaultExceptionHandler", exception);
+
+		return mv;
+	}
+
 	@org.springframework.web.bind.annotation.ExceptionHandler(Exception.class)
 	public ModelAndView defaultExceptionHandler(HttpServletRequest request, Exception exception){
 		ModelAndView mv = new ModelAndView("/errors/error");
@@ -19,5 +52,6 @@ public class ExceptionHandler {
 		
 		return mv;
 	}
-
 }
+
+
